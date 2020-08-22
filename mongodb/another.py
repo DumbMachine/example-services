15b4a87a3769f3a0aea5feb3a47e4d34ecd6b303
DumbMachine <ratin.kumar.2k@gmail.com>
2020-08-23 01:22:56 +0530

    temp

--- mongodb/another.py
+++ mongodb/another.py
@@ -11,18 +11,18 @@ def test_keys():
     else:
         assert False
 
-def test_one():
-    from pymongo import MongoClient
-    from pymongo.errors import ServerSelectionTimeoutError
-    PORT = os.environ["MONGODB_PORT"] if "MONGODB_PORT" in os.environ else 27017
-    HOST = os.environ["MONGODB_HOST "] if "MONGODB_HOST " in os.environ else "localhost"
-    connection_string = f"mongodb://{HOST}:{PORT}/"
-    client = MongoClient(connection_string, serverSelectionTimeoutMS=10, connectTimeoutMS=20000)
-    try:
-        _ = client.server_info() # Forces a call.
-        assert True
-    except ServerSelectionTimeoutError:
-        assert False
+# def test_one():
+#     from pymongo import MongoClient
+#     from pymongo.errors import ServerSelectionTimeoutError
+#     PORT = os.environ["MONGODB_PORT"] if "MONGODB_PORT" in os.environ else 27017
+#     HOST = os.environ["MONGODB_HOST "] if "MONGODB_HOST " in os.environ else "localhost"
+#     connection_string = f"mongodb://{HOST}:{PORT}/"
+#     client = MongoClient(connection_string, serverSelectionTimeoutMS=10, connectTimeoutMS=20000)
+#     try:
+#         _ = client.server_info() # Forces a call.
+#         assert True
+#     except ServerSelectionTimeoutError:
+#         assert False
 
 
 
